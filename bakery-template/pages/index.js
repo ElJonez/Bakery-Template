c70f9c82b9c2f353b6ead5df8b13d12ea6e39865
ElJonez <adamhjonesbackup@gmail.com>
2021-04-03 17:35:53 +0100

    changed Index

--- bakery-template/pages/index.js
+++ bakery-template/pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <div>
+            Welcome to El Bakery
+          </div>
         </h1>
 
         <p className={styles.description}>
